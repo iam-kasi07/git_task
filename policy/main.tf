a7fdc9398f36c4e0cacd0fbf9f014b938e1f4ae2
venkatasai.murala@gmail.com <venkatasai.murala@gmail.com>
2025-01-03 13:46:38 +0530

    sai

--- policy/main.tf
+++ policy/main.tf
@@ -13,22 +13,22 @@ module "secret_rotate" {
   )
 }
 
-module "secret" {
-  source = "../secret/"
+# # module "secret" {
+# #   source = "../secret/"
 
-  short_prefix            = "${var.short_prefix}-api-key"
-  long_prefix             = "${var.long_prefix}-api-key"
-  rotate                  = var.key_rotation_days != null
-  lambda_key_rotation_arn = var.key_rotation_days != null ? module.secret_rotate[0].arn : null
-  write_roles             = var.write_roles
-  read_roles              = var.read_roles
-  administrator_roles     = var.administrator_roles
-  key_rotation_days       = var.key_rotation_days
-  # tags = merge(
-  #   var.tags,
-  #   {
-  #     "Description" = "API Key for ${var.title}"
-  #   }
-  # )
-  kms = var.kms
-}
+# #   short_prefix            = "${var.short_prefix}-api-key"
+# #   long_prefix             = "${var.long_prefix}-api-key"
+# #   rotate                  = var.key_rotation_days != null
+# #   lambda_key_rotation_arn = var.key_rotation_days != null ? module.secret_rotate[0].arn : null
+# #   write_roles             = var.write_roles
+# #   read_roles              = var.read_roles
+# #   administrator_roles     = var.administrator_roles
+# #   key_rotation_days       = var.key_rotation_days
+# #   # tags = merge(
+# #   #   var.tags,
+# #   #   {
+# #   #     "Description" = "API Key for ${var.title}"
+# #   #   }
+# #   # )
+# #   kms = var.kms
+# # }
