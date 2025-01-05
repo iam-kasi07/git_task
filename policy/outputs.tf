5d064c0fd9d6cc347024458a3df684b1bb7621f5
jahnavi <jahnavicherukuri3@gmail.com>
2025-01-05 17:44:54 +0530

    pull request

--- policy/outputs.tf
+++ policy/outputs.tf
@@ -6,17 +6,17 @@ output "statements" {
   )
 }
 
-output "assume_statements" {
-  value = concat(
-    module.api_key_secret_rotate_policy.assume_statements,
-    [
-    ]
-  )
-}
+# output "assume_statements" {
+#   value = concat(
+#     module.api_key_secret_rotate_policy.assume_statements,
+#     [
+#     ]
+#   )
+# }
 
-output "long_prefix" {
-  value = var.long_prefix
-}
+# output "long_prefix" {
+#   value = var.long_prefix
+# }
 
 output "short_prefix" {
   value = var.short_prefix
